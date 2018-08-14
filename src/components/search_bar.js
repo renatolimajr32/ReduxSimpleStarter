685bb87744d2dc96de772591ef0bd5a766a19ca9
Renato Lima Júnior <rlimajr@alta-rt.com>
2018-08-14 13:38:02 -0300

    finishing the app

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,9 +10,16 @@ class SearchBar extends Component {
   }
 
   render() {
-    return (<div>
-      <input value={this.state.term} onChange={event => this.setState({term: event.target.value})}/>
+    return (<div className="search-bar">
+      <input
+        value={this.state.term}
+        onChange={event => this.onInputChange(event.target.value)}/>
     </div>);
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
